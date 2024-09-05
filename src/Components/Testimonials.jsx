7ae26576cf64ce2cae35be13d7a2a437b4790c13
jeffren <ijeffren1@gmail.com>
2024-09-05 19:35:53 +0100

    deployment issue

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Users1 from './images/Users1.avif'
 import Users2 from './images/Users2.jpg'
 import Users3 from './images/Users3.avif'
-import './Testimonials.css' 
+import './Testimonials.css'
 
 const Testimonials = () => {
   return (
@@ -16,14 +16,14 @@ const Testimonials = () => {
             <p>In just 2 very short meetings with John he manage to find the solution personally catered to my solution and expectations. punctual, will informed and very reliable. </p>
             <p><span>Tasha patrick</span></p>
             <p>Director at Risktec Solutions Ltd</p>
-       </div>
-        <div className='card'>
+          </div>
+          <div className='card'>
             <img src={Users2} alt='Users2.avif' />
             <p>it is not day that you come across a passionate and trustworthy financial advisor John Doe is the professional who does his work really well Thanks John  </p>
             <p><span>Jerry .M.J</span></p>
             <p>Managing Director of BPW Globa </p>
-       </div>
-        <div className='card'>
+          </div>
+          <div className='card'>
             <img src={Users1} alt='Users1.avif' />
             <p> i have a professional financial advisor, who is true to his word John has demonstrated a high amount of integrity in the time that i have known him, and he is fast to respond </p>
             <p><span>Maureen J.P </span></p>
@@ -31,7 +31,7 @@ const Testimonials = () => {
           </div>
         </div>
       </div>
-      
+
     </div>
   )
 }
